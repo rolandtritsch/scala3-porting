ab66bd08129c1b5417d2dccca701112571c0cae9
Roland Tritsch <roland@tritsch.org>
2021-03-01 15:26:35 +0000

    Yaml: scala3.

---
+++ yaml/scala3/src/main/scala/Yaml.scala
@@ -0,0 +1,13 @@
+import net.jcazevedo.moultingyaml._
+import net.jcazevedo.moultingyaml.DefaultYamlProtocol._
+
+object Yaml {
+  def main(args: Array[String]): Unit = {
+    require(args.size == 1, "Usage: Yaml <fileName>")
+    val fileName = args(0)
+
+    val yamlString = scala.io.Source.fromFile(fileName).mkString
+    val yamlAst = yamlString.parseYaml
+    println(yamlAst.prettyPrint)
+  }
+}
