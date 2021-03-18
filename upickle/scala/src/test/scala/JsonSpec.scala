014511d0cfa7896a953f61a9d66afee246a33dff
Roland Tritsch <roland@tritsch.org>
2021-03-18 08:47:29 +0000

    upickle: scala: Adding singularity model.

--- upickle/scala/src/test/scala/JsonSpec.scala
+++ upickle/scala/src/test/scala/JsonSpec.scala
@@ -9,13 +9,7 @@ class JsonSpec extends AnyFlatSpec with should.Matchers {
 
   "reading resources.json" should "convert to case class" in {
     val json = scala.io.Source.fromResource("resources.json").mkString
-    val expected = SingularityResources(Some(0.1), 2.0, 3, Some(4))
-    read[SingularityResources](json) should be (expected)    
-  }
-
-  "reading resources.json" should "convert to case class (with option)" in {
-    val json = scala.io.Source.fromResource("resources-with-option.json").mkString
-    val expected = SingularityResources(None, 2.0, 3, None)
+    val expected = SingularityResources(0.1, 2.0, 3, 4)
     read[SingularityResources](json) should be (expected)    
   }
 
